e40b920ad464b9df506b37b5ac16e782f7ecde0f
didi <1572069841@qq.com>
2022-05-15 18:03:02 +0800

    np

--- dev.py
+++ dev.py
@@ -1,4 +1,4 @@
-
+## Write only functions, not classes if necessary
 # def ej():
 #     import pathlib
 #     exec(pathlib.Path("/Users/didi/Desktop/repos/grl/dev.py").open('r').read(), globals())
@@ -417,6 +417,37 @@ def _assign_gps(df):
 
 #### now, start to compile busgraph igraph
 
+def _np_distance_args_preprocess(*args):
+    assert len(args) in (1, 4)
+    if args.__len__() == 4:
+        lng1, lat1, lng2, lat2 = args
+    if args.__len__() == 1:
+        gps_arr = args[0]
+        lng1, lat1, lng2, lat2 = [gps_arr[:, i] for i in range(4)]
+    return lng1, lat1, lng2, lat2
+
+def np_distance(*args):
+    lng1, lat1, lng2, lat2 = _np_distance_args_preprocess(*args)
+    radius = 6371
+    dlng = np.radians(lng2-lng1)
+    dlat = np.radians(lat2-lat1)
+    a = np.sin(dlat/2)*np.sin(dlat/2) \
+        + np.cos(np.radians(lat1)) * np.cos(np.radians(lat2)) * np.sin(dlng/2) * np.sin(dlng/2)
+    c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
+    d = radius * c
+    return d
+def np_coords(*args):
+    lng1, lat1, lng2, lat2 = _np_distance_args_preprocess(*args)
+    sx = np.copysign(1, lng1-lng2)
+    sy = np.copysign(1, lat1-lat2)
+    x = np_distance(lng1, lat1, lng2, lat1)
+    y = np_distance(lng1, lat1, lng1, lat2)
+    return np.stack([sx*x, sy*y], axis=0).T
+def np_mht(*args):
+    r = np_coords(*args)
+    y = np.abs(r[:, 0]) + np.abs(r[:, 1])
+    return y
+
 def fill_empty_gc_for_gate_sid(row):
     gc = row.gc
     gate = row.gate
@@ -424,3 +455,5 @@ def fill_empty_gc_for_gate_sid(row):
         gc = str([row.lng, row.lat])
     return gc
 
+## use kdtree
+from scipy.spatial import KDTree
